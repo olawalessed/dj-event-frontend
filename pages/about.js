807eb48225eab3d972c70f03b2d5177989e741f3
Olawalessed <51535503+OlawaleSOD@users.noreply.github.com>
2021-08-10 18:40:11 +0100

    Create Layout Header and Footer

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is the app helps you see a list of DJ events nearby</p>
+        </div>
+    )
+}
