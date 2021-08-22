83b5404c4c1641c27a48f5e212e53fad108292a1
Olawalessed <51535503+OlawaleSOD@users.noreply.github.com>
2021-08-22 12:15:45 +0100

    Added strapi login

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-import { API_URL } from '@/config/index'
+import { NEXT_URL } from '@/config/index'
 
 //Create the authentication context
 const AuthContext = createContext()
@@ -19,7 +19,26 @@ export const AuthProvider = ({children}) => {
 
     //Login User
     const login = async ({ email:identifier, password }) => {
-        console.log({identifier, password})
+        //console.log({identifier, password})
+      const res = await fetch(`${NEXT_URL}/api/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          identifier, password
+        })
+      })
+
+      const data = await res.json()
+      
+      console.log(data)
+      
+      if (res.ok) {
+        setUser(data.user)
+      } else {
+        setError(data.message)
+      }
     }
  
     //Logout user
